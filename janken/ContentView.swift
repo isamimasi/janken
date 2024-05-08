fe5f552d2255532326ca65a118fac4510987188a
isamimasi <41987332+isamimasi@users.noreply.github.com>
2024-05-08 16:45:22 +0900

    Initial Commit

---
+++ janken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  janken
+//
+//  Created by F. ISAMI. on 2024/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by F. ISAMI. on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
