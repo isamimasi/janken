b634b00e2f580dead5e429ac37c645bf978783bc
isamimasi <41987332+isamimasi@users.noreply.github.com>
2024-05-08 18:30:17 +0900

    added tag

--- janken/ContentView.swift
+++ janken/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    // jankenNumber を定義する。スタート０　ぐーは１　チョキは２　パーは３
     @State var jankenNumber = 0
     var body: some View {
         VStack {
             Spacer()
+            //スタート画面
             if jankenNumber==0 {
                 Text("これからジャンケンをします！")
                     .padding(.bottom)
             }
+            // グーの場合
             else if jankenNumber==1 {
                 Image(.gu)
                     .resizable()
@@ -23,6 +26,7 @@ struct ContentView: View {
                 Spacer()
                 Text("グー")
                 .padding(.bottom);}
+            // チョキの場合
             else if jankenNumber==2 {
                 Image(.choki)
                     .resizable()
@@ -31,6 +35,7 @@ struct ContentView: View {
                 Text("チョキ")
                     .padding(.bottom);
             }
+            // パーの場合
             else {
                 Image(.pa)
                     .resizable()
@@ -39,9 +44,14 @@ struct ContentView: View {
                 Text("パー")
                     .padding(.bottom);
             }
+            // ボタンの定義　タップされたらアクション
             Button(action:{
                 print ("tapped!")
+                //　newJankenNumber　を定義する前回の手とダブらないようにしている
                 var newJankenNumber = 0;
+                // repeat while 構文
+                // repeat (繰り返す)　while この条件の場合は繰り返す。
+                // jankenNumber == newJankenNumberは繰り返す。
                 repeat {
                     newJankenNumber = Int.random(in: 1...3);
                 }
